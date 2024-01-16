66ed55875a9313517c63dfb80d5bfdbdd6839ce0
ondogan <156133593+ondogan@users.noreply.github.com>
2024-01-15 18:03:14 -0800

    Session 1 prompt for Coding Journal 1

---
+++ astro19-prompt1.py
@@ -0,0 +1,11 @@
+def print_onat():
+    print("Onat Dogan")
+    print("He/Him")
+    print("Favorite movie is Pulp Fiction")
+    print("I like different kebabs and seafood")
+
+def main():
+    print_onat()
+
+if __name__ =="__main__":
+    main()
