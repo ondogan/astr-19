1ac3a583a8122c7bf34314438c5df030a28dd6ef
ondogan <156133593+ondogan@users.noreply.github.com>
2024-01-26 11:51:26 -0800

    Session 2 prompt for Coding Journal 1

---
+++ Session-2-prompt.py
@@ -0,0 +1,31 @@
+import math
+
+# define two floats
+
+x = 7.2
+y = 10.1
+
+
+
+#define two integers
+
+a = 5
+b = -7
+
+# print the sum of x and y and its class
+
+print(x+y)
+print(type(x+y))
+
+
+#print  the difference between a and b and its class
+
+print(a-b)
+
+print(type(a-b))
+
+
+#print integer times float so in this case do y times b and print its class
+
+print(y*b)
+print(type(y*b))
